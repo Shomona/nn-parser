98db8055fc4baf4919a86670c11b72a48aaf4b0a
Shomona <shomona.mukherjee@gmail.com>
2019-03-03 21:04:18 -0500

    allow-origin

--- config/env/production.js
+++ config/env/production.js
@@ -250,10 +250,11 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
+     onlyAllowOrigins: [
+       'https://nn-parser.herokuapp.com'
     //   'https://example.com',
     //   'https://staging.example.com',
-    // ],
+     ],
 
 
     /***************************************************************************
